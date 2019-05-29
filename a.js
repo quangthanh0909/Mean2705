b48403bd9366e7841a6c5ce47c3aac328714424d
thanhuyen <thanhuyen1703@gmail.com>
2019-05-29 16:31:03 +0700

    a.js

---
+++ a.js
@@ -0,0 +1,59 @@
+// let a =1
+// var b='string';
+// console.log(a)
+
+/**
+ * Khai báo hằng số a 
+ */
+
+
+// Đưa con trỏ vào a sẽ hiện ra comment, thường dùng khai báo để cho co-worker có thể hiểu được ghi chú
+
+// Type of variable: Number, string, Boolean,Array,null, undefined
+
+// declaration Array
+/**
+ * Falsy data : 0 , false, null, undefined, NaN
+ * Mang rong la true if([]) -> true
+ */
+
+  // cách tạo khóa key trong array 
+ // array là object 
+//  let arr1 = new Array(1,3,4);
+//  console.log(arr1);
+
+ // push, pop, shift, unshift
+ // find,findIndex, ....
+// let arr=[
+//     {id:2,name:"Thanh"},
+//     {id:1,name:"Uyen"},
+// ] 
+
+
+//  let element = arr.find(v=>v.id ===2)
+//  console.log('element:', element);
+let arr = new Array(1,2,3,5,6,7,5);
+let arr1 = new Array("a","b",3);
+
+let newarr =arr.concat("a","b");
+console.log(arr);
+console.log('newarr:', newarr)
+let arr3 = arr.entries();
+console.log('arr3:', arr3.next().value)
+console.log('arr3:', arr3.next().value)
+console.log('arr3:', arr3.next().value)
+console.log('arr3:', arr3.next().value)
+console.log('arr3:', arr3.next().value)
+
+
+
+
+
+ 
+
+
+ 
+
+
+
+
